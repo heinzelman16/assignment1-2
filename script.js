d6cdfb632e9df1cf0f34c39771f1e77211a7a52a
christopehr heinzelman <heinzelman_christopher@columbusstate.edu>
2021-03-29 22:24:51 +0000

    created mobile site

---
+++ script.js
@@ -0,0 +1,10 @@
+
+<script>
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+</script>
